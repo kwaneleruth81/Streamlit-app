559e01a4016f6da0b315a60776e45034abef1378
kwaneleruth81 <kwanelesciency@gmail.com>
2024-06-07 18:44:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
-input_data = user_input_features()
+#input_data = user_input_features()
 
 # Display user input
 st.subheader('User Input Parameters')
