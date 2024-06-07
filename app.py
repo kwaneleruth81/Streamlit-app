faed191e20f6ffb02d3c302b176760a6d7ad12ae
kwaneleruth81 <kwanelesciency@gmail.com>
2024-06-07 18:41:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 #from sklearn.model_selection import train_test_split
 #from sklearn.ensemble import RandomForestRegressor
 #from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 path = r"C:\Users\Admin\Downloads\HPS_data.csv"
 # Load dataset
